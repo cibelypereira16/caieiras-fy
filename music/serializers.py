af2d29ae5ea59ec48500f82ea5d511fdda8a3bc5
emanuelle <52548760+manuzinha0901@users.noreply.github.com>
2019-09-16 15:23:15 -0300

    Update serializers.py

--- music/serializers.py
+++ music/serializers.py
@@ -6,4 +6,5 @@ from music.models import Music
 class MusicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Music
-        fields =('name_music', 'artist', 'genre', 'link_music' )
+        fields =('name_music', 'artist', 'genre', 'link_music','comida')
+        
